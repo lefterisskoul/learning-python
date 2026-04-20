ab4a7d4fc7475ec444cd3c8f06b104fccc045305
Lefteris Skouloukas <warel2005@gmail.com>
2026-04-20 11:39:57 +0300

    initial commit

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,10 +1,6 @@
-#city_name = input("Welcome to the Band Name Generator." + "\n" + "What's the name of the city you grew up in?" + "\n")
-#name = city_name
-#pet_name = input("What's your pet's name?" + "\n")
-#pname = pet_name
-#print("Your band name could be:", city_name + " " + pname)
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-print ("Welcome to the Band Name Generator")
-city = input("Which city did you grow up in?\n")
-pet = input("What is the name of your pet?\n")
-print("Your band name could be: " + city + " " + pet)
+# Write your code below this line 👇
+#print("Hello world!\nHello world!\nHello world!")
+
+#print("Hello" + "Artemis")
+
+print("Hello" + " " + "Lefteris")
